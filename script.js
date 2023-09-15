c84c4a33f2d927c8a524927ec68c6dad54dc9e9d
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-09-15 23:30:19 +0300

    inches-to-meters

---
+++ script.js
@@ -0,0 +1,20 @@
+const Calc = document.querySelector("button")
+const resultconvert = document.querySelector(".result")
+const form = document.querySelector("form")
+
+function convert(x){
+    return x * 0.0254
+}
+function display(finalResult){
+    resultconvert.textContent = finalResult
+}
+function handleSubmit(){
+    display(convert(inches.value))
+}
+
+form.addEventListener( "submit", function(e){
+    e.preventDefault()
+    handleSubmit() 
+    
+
+} )
